4a3b1a9c3219fc804ee905fb2ef7da3b2c3e71ed
alvin citaka <alvin.citaka@allianz.co.id>
2020-02-23 14:44:09 +0700

    Add usage documentation

--- ImageToTextLibrary/Keywords.py
+++ ImageToTextLibrary/Keywords.py
@@ -6,6 +6,15 @@ class Keywords():
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
     def get_text_from_image(self, image_file_path):
+        """
+        Get text of an image
+
+        Args:
+         - image_file_path - the absolute path of the image
+
+        Usage:
+        | ${text}= | Get Text From Image | ${CURDIR}/doc/robot/sample-image.png |
+        """
         if not image_file_path:
             raise Exception('Parameter image_file_path is required')
 
--- ImageToTextLibrary/__init__.py
+++ ImageToTextLibrary/__init__.py
@@ -6,11 +6,17 @@ __version__ = VERSION
 
 
 class ImageToTextLibrary(Keywords):
+    """ImageToTextLibrary is robotframework keyword library to obtain text in an image
+
+            = Prerequisite =
+
+            The library uses Tesseract to convert the image into text.
+            The machine has to be able to execute command `tesseract`.
+            Follow the installation link : https://github.com/tesseract-ocr/tesseract
+        
+        """
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     ROBOT_LIBRARY_VERSION = VERSION
 
     def __init__(self):
-        '''ImageToTextLibrary can be imported.
-        '''
-        vs = VERSION
-        print ("Image to Text Library imported")
+        pass
